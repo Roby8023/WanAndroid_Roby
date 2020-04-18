c5bfbad71e1b1e693d0c1e771d71d802779f6ab8
Roby <huolu8023@163.com>
2020-04-18 11:52:20 +0800

    Revert code to resource

--- basic_core/src/main/java/per/goweii/basic/core/glide/progress/ProgressAppGlideModule.java
+++ basic_core/src/main/java/per/goweii/basic/core/glide/progress/ProgressAppGlideModule.java
@@ -10,11 +10,11 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
-import com.readystatesoftware.chuck.ChuckInterceptor;
 
 import java.io.InputStream;
 
 import okhttp3.OkHttpClient;
+import per.goweii.basic.utils.LogUtils;
 import per.goweii.rxhttp.request.utils.HttpsCompat;
 
 /**
@@ -28,7 +28,8 @@ public class ProgressAppGlideModule extends AppGlideModule {
 
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
-        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(getOkHttpClient(context)));
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(getOkHttpClient()));
+        LogUtils.d("ProgressAppGlideModule", "registerComponents down");
     }
 
     @Override
@@ -36,11 +37,13 @@ public class ProgressAppGlideModule extends AppGlideModule {
         return false;
     }
 
-    private static OkHttpClient getOkHttpClient(Context context) {
+    private static OkHttpClient getOkHttpClient() {
+        LogUtils.d("ProgressAppGlideModule", "getOkHttpClient");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpsCompat.ignoreSSLForOkHttp(builder);
         HttpsCompat.enableTls12ForOkHttp(builder);
-        builder.addInterceptor(new ProgressInterceptor())
-                .addInterceptor(new ChuckInterceptor(context).showNotification(true));
+        builder.addInterceptor(new ProgressInterceptor());
+        LogUtils.d("ProgressAppGlideModule", "getOkHttpClient down");
         return builder.build();
     }
 }
